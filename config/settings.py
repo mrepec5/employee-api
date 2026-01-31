27a0588e735460ad792a17ed276b1fbb2d9cbe60
Mateusz Repeć <repec.mateusz@gmail.com>
2026-01-31 07:42:36 +0100

    feat: Changed permissions to IsAuthenticated.

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PERMISSION_CLASSES': [
-            'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+            'rest_framework.permissions.IsAuthenticated',
     ],
 }
 
